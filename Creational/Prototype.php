0e3e74b6fe6985b386270681877905b9482f4390
Ali Kamal <alikamal1@yahoo.com>
2019-10-04 13:23:17 +0300

    prototype

--- Creational/Prototype.php
+++ Creational/Prototype.php
@@ -2,18 +2,37 @@
 
 namespace Creational\Prototype;
 
-class page
+use function Creational\Factory_Method\clientCode as CreationalClientCode;
+
+/**
+ * Prototype Design Pattern
+ * let you copy existing objects without making your code dependent on their classes
+ */
+
+class Author
+{
+    private $name;
+    private $pages = [];
+
+    public function __construct(string $name) {
+        $this->name = $name;
+    }
+
+    public function addToPage(Page $page): void
+    {
+        $this->pages[] = $page;
+    }
+}
+
+class Page 
 {
     private $title;
     private $body;
-
     private $author;
     private $comments = [];
-
     private $date;
-
-    public function __construct(string $title, string $body, Author $author)
-    {
+    
+    public function __construct(string $title, string $body, Author $author) {
         $this->title = $title;
         $this->body = $body;
         $this->author = $author;
@@ -35,30 +54,13 @@ class page
     }
 }
 
-class Author
-{
-    private $name;
-    private $pages = [];
-
-    public function __construct(string $name)
-    {
-        $this->name = $name;
-    }
-
-    public function addToPage(Page $page): void
-    {
-        $this->pages[] = $page;
-    }
-}
-
 function clientCode()
 {
     $author = new Author("Ali Kamal");
-    $page = new Page("Tip of the day", "Keep calm and carry on.", $author);
-    $page->addComment("Nice top, thanks!");
-    
+    $page = new Page("Tip of the day", "Keep calm and carry on", $author);
+    $page->addComment("Nice tip, thanks");
+
     $draft = clone $page;
-    echo "Dump of the clone. Note that the author is now referencing two objects. <br><br>";
     print_r($draft);
 }
 
